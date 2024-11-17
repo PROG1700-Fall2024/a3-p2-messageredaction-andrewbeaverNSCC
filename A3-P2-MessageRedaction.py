2698a3d2ac816e302708526cac58691b2ad9a6c2
Your Name <w0402993@nscc.ca>
2024-11-16 22:14:24 -0400

    Almost done, more functions

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -2,19 +2,48 @@
 #Description:   Design and write a program that counts and removes all desired letters from 
 #               any user-entered sentence or phrase.
 
-#Student #:     
-#Student Name:  
+#Student #:     W0402993
+#Student Name:  Andrew Beaver
+
+def x():
+    redactCount = 0
+    while True:
+        program = input("\nType a phrase (or quit to exit program): ").lower()
+        if program == "quit":
+            print("End of program")
+            break
+        lettersToRedact = input("Type a comma-seperated list of letters to redact: ").lower()
+        redaction = lettersToRedact.split(',')
+
+        for letter in redaction:
+            redactedProgram = program.replace(letter, "_")
+            redactCount += program.count(letter)
+            program = redactedProgram
+        print("Numbers of letters redacted: {0}".format(redactCount))
+        print("Redacted Phrase: {0}".format(redactedProgram))
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
 
+    #redactCount = 0
+    print("Message Redaction")
 
+    # while True:
+    #     program = input("\nType a phrase (or quit to exit program): ").lower()
+    #     if program == "quit":
+    #         print("End of program")
+    #         break
+    #     lettersToRedact = input("Type a comma-seperated list of letters to redact: ").lower()
+    #     redaction = lettersToRedact.split(',')
 
+    #     for letter in redaction:
+    #         redactedProgram = program.replace(letter, "_")
+    #         redactCount += program.count(letter)
+    #         program = redactedProgram
 
-
-
-
-    # YOUR CODE ENDS HERE
+        
+    #     print("Numbers of letters redacted: {0}".format(redactCount))
+    #     print("Redacted Phrase: {0}".format(redactedProgram))
+    x()
 
 main()
