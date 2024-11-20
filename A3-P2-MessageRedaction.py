5b667bae071fed25bb7222f59c17aa7b89b435e3
Your Name <w0402993@nscc.ca>
2024-11-19 22:20:15 -0400

    Ready for marking

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -5,16 +5,26 @@
 #Student #:     W0402993
 #Student Name:  Andrew Beaver
 
-def x():
+def openingMessage():
+    return "Message Redaction"
+
+def redactionFunction():
     redactCount = 0
+
+    #Create infinite loop, quit ends program
     while True:
+
+        #Get phrase from user
         program = input("\nType a phrase (or quit to exit program): ").lower()
         if program == "quit":
             print("End of program")
             break
+
+        #Get the characters to redact from user. use split() to get the characters in the same input
         lettersToRedact = input("Type a comma-seperated list of letters to redact: ").lower()
         redaction = lettersToRedact.split(',')
 
+        #replace characters with _
         for letter in redaction:
             redactedProgram = program.replace(letter, "_")
             redactCount += program.count(letter)
@@ -24,26 +34,9 @@ def x():
 
 def main():
 
+    message = openingMessage()
+    print(message)
 
-    #redactCount = 0
-    print("Message Redaction")
-
-    # while True:
-    #     program = input("\nType a phrase (or quit to exit program): ").lower()
-    #     if program == "quit":
-    #         print("End of program")
-    #         break
-    #     lettersToRedact = input("Type a comma-seperated list of letters to redact: ").lower()
-    #     redaction = lettersToRedact.split(',')
-
-    #     for letter in redaction:
-    #         redactedProgram = program.replace(letter, "_")
-    #         redactCount += program.count(letter)
-    #         program = redactedProgram
-
-        
-    #     print("Numbers of letters redacted: {0}".format(redactCount))
-    #     print("Redacted Phrase: {0}".format(redactedProgram))
-    x()
+    redactionFunction()
 
 main()
